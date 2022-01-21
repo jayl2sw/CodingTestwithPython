bf7e722b9aed4d8a4be4aae6f13fa85f8459ef4b
jayl2sw <jayl2sf@gmail.com>
2022-01-21 17:54:47 +0900

    practice

--- Chapter5_DFS,BFS/5-10_음료수얼려먹기_.py
+++ Chapter5_DFS,BFS/5-10_음료수얼려먹기_.py
@@ -1,31 +1,29 @@
 n, m = map(int, input().split())
 
-v = [[0] * m for _ in range(n)]
-
 graph = []
-for i in range(n):
-    graph.append(list(map(int,input())))
+for _ in range(n):
+    graph.append(list(map(int, input())))
 
 
-def dfs(x, y):
+def dfs(x,y):
     if x < 0 or y < 0 or x >= n or y >= m:
         return False
-    if graph[x][y] == 0:
-        graph[x][y] =1
-        dfs(x + 1, y)
-        dfs(x - 1, y)
-        dfs(x, y + 1)
-        dfs(x, y - 1)
-        return True
+    else:
+        if graph[x][y] == 0:
+            graph[x][y] = 1
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+            return True
+
     return False
 
 
-result = 0
+count = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i, j):
-            result += 1
+        if dfs(i,j):
+            count += 1
 
-print(result)
-
-
+print(count)
--- Chapter5_DFS,BFS/5-11_미로탈출_.py
+++ Chapter5_DFS,BFS/5-11_미로탈출_.py
@@ -1,33 +1,49 @@
 from collections import deque
 
-n, m = map(int,input().split())
+n, m = map(int, input().split())
 
 graph = []
+
 for i in range(n):
     graph.append(list(map(int,input())))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, -0, 1]
+
+print(graph)
 
 def bfs(x, y):
-    queue = deque()
-    queue.append((x,y))
-    while queue:
-        x, y = queue.popleft()
+    q = deque()
+    q.append((x,y))
+
+    while q:
+        x, y = q.popleft()
+        print(x,y)
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx <0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
+              continue
+
             if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
+               graph[nx][ny] = graph[x][y] + 1
+               q.append((nx, ny))
+
+    for i in graph:
+        print(i)
     return graph[n-1][m-1]
 
 
+
+
+
 print(bfs(0,0))
+
+
+
+
+
 
 
 
