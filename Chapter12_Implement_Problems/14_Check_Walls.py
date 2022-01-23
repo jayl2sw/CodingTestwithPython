ff6bd9148d76fc68a48160571e94a4c68993aab0
jayl2sw <jayl2sf@gmail.com>
2022-01-23 22:19:26 +0900

    1day

--- Chapter12_Implement_Problems/14_Check_Walls.py
+++ Chapter12_Implement_Problems/14_Check_Walls.py
@@ -5,10 +5,3 @@ array = [1,3,4,9,10]
 dist = [1,2,3,4]
 differences = []
 
-for i in range(len(array)-1):
-    difference = array[i+1] - array[i]
-    differences.append(difference)
-
-differences.append(array[0] + n - array[-1])
-
-print(differences)
